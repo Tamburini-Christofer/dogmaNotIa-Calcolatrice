89edbeb576f676da0aba1dc49ca33ef85b5cd3bc
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-02-05 16:54:11 +0100

    Creazione stile base

--- src/assets/Components/Calcolatrice.jsx
+++ src/assets/Components/Calcolatrice.jsx
@@ -3,20 +3,25 @@ export default function Calcolatrice () {
         <>
         <div id="contenitoreCalcolatrice">
             <div className="contenitoreRisultato">
+                
             </div>
             <div className="contenitoreOperandi">
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
-                <div className="celleOperandi"></div>
+                <div className="celleOperandi">7</div>
+                <div className="celleOperandi">8</div>
+                <div className="celleOperandi">9</div>
+                <div className="celleOperandi">X</div>
+                <div className="celleOperandi">4</div>
+                <div className="celleOperandi">5</div>
+                <div className="celleOperandi">6</div>
+                <div className="celleOperandi">-</div>
+                <div className="celleOperandi">1</div>
+                <div className="celleOperandi">2</div>
+                <div className="celleOperandi">3</div>
+                <div className="celleOperandi">+</div>
+                <div className="celleOperandi">+/-</div>
+                <div className="celleOperandi">0</div>
+                <div className="celleOperandi">,</div>
+                <div className="celleOperandi">=</div>
             </div>
         </div>
         </>
